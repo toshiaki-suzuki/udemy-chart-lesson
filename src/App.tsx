23cfae90338df6ad9ed156f6ba6451ae563fa087
akis <flaflabell@gmail.com>
2022-03-30 18:46:20 +0900

    棒グラフ

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import LinePlot from "./component/LinePlot";
 import './App.css';
 import PiePlot from './component/PiePlot';
+import BarPlot from './component/BarPlot';
 
 function App() {
   return (
     <div className="App">
       <LinePlot/>
       <PiePlot/>
+      <BarPlot/>
     </div>
   );
 }
