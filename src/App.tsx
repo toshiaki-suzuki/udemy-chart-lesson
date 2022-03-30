495a39952fc295d70633c6a3f10bd9898089fa75
akis <flaflabell@gmail.com>
2022-03-30 18:42:19 +0900

    円グラフ

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LinePlot from "./component/LinePlot";
 import './App.css';
+import PiePlot from './component/PiePlot';
 
 function App() {
   return (
     <div className="App">
       <LinePlot/>
+      <PiePlot/>
     </div>
   );
 }
